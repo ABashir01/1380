f0b317826f31ba604d6245a73acb16050f0b90ed
Ahad Bashir <54181608+ABashir01@users.noreply.github.com>
2025-02-12 15:11:00 -0500

    Add files via upload

--- latency_test.js
+++ latency_test.js
@@ -8,18 +8,36 @@ const obs = new PerformanceObserver((items) => {
     performance.clearMarks();
     if (items.getEntries().length === 10) {
         console.log('Average duration:', totalDuration / 10);
-    }    performance.clearMarks();
+    } else if (items.getEntries().length === 1000) {
+        console.log('Average duration:', totalDuration / 1000);
+    }
+    
+    performance.clearMarks();
     });
 
 obs.observe({ entryTypes: ['measure'] });
 
-let totalDuration = 0;
-for (let i = 0; i < 10; i++) {
-    performance.mark('A');
-    const num = 12;
-    const serialized = distribution.util.serialize(num);
-    const deserialized = distribution.util.deserialize(serialized);
-    performance.mark('B');
-    performance.measure('A to B', 'A', 'B');
-}
+// M1 Latency Measure
+// let totalDuration = 0;
+// for (let i = 0; i < 10; i++) {
+//     performance.mark('A');
+//     const num = {a: 1, b: 2, c: 3};
+//     const serialized = distribution.util.serialize(num);
+//     const deserialized = distribution.util.deserialize(serialized);
+//     performance.mark('B');
+//     performance.measure('A to B', 'A', 'B');
+// }
 
+// M2 Latency Measure
+const node = distribution.node.config;
+const remote = {node: node, service: 'status', method: 'get'};
+const message = ['nid'];
+
+let totalDuration = 0;
+for (let i=0; i<1000; i++) {
+    performance.mark('A');
+    distribution.local.comm.send(message, remote, (e, v) => {
+        performance.mark('B');
+        performance.measure('A to B', 'A', 'B');
+    });
+}
